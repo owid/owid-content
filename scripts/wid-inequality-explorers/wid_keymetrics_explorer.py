1abecc6373562865577086b8c54b2ff899d51be0
Pablo Arriagada <paarriagadap@gmail.com>
2023-02-09 16:23:54 +0000

    feat(wid): Incomes across the distribution

--- scripts/wid-inequality-explorers/wid_keymetrics_explorer.py
+++ scripts/wid-inequality-explorers/wid_keymetrics_explorer.py
@@ -38,7 +38,7 @@ tables = pd.read_csv(url, keep_default_na=False)
 # %%
 # The header is defined as a dictionary first and then it is converted into a index-oriented dataframe
 header_dict = {
-    "explorerTitle": "Inequality Data Explorer of the World Inequality Database",
+    "explorerTitle": "Key Metrics Explorer of the World Inequality Database",
     "selection": [
         "Chile",
         "Brazil",
