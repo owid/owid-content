e3d57f7bbca354bdf8123988d308267bf7d3eefe
Pablo Arriagada <paarriagadap@gmail.com>
2023-02-10 14:06:14 +0000

    Add `pickerColumnSlugs`

--- scripts/wid-inequality-explorers/wid_incomes_across_distribution_explorer.py
+++ scripts/wid-inequality-explorers/wid_incomes_across_distribution_explorer.py
@@ -67,6 +67,7 @@ header_dict = {
     "googleSheet": f"https://docs.google.com/spreadsheets/d/{sheet_id}",
     "wpBlockId": "",
     "entityType": "country or region",
+    "pickerColumnSlugs": "p0p100_avg_pretax p0p100_avg_posttax_dis p0p100_avg_posttax_nat p0p100_avg_wealth",
 }
 
 # Index-oriented dataframe
