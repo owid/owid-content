d8bea69afc861f2194afedad25e56a878b3bce9e
Pablo Arriagada <paarriagadap@gmail.com>
2023-02-17 17:39:37 +0000

    fix(lis): Correct title

--- scripts/lis-poverty-and-inequality-explorers/lis_expanded_poverty_explorer.py
+++ scripts/lis-poverty-and-inequality-explorers/lis_expanded_poverty_explorer.py
@@ -54,7 +54,7 @@ tables = pd.read_csv(url, keep_default_na=False)
 # %%
 # The header is defined as a dictionary first and then it is converted into a index-oriented dataframe
 header_dict = {
-    "explorerTitle": "Key Metrics Explorer of the Luxembourg Income Study",
+    "explorerTitle": "Poverty Data Explorer of the Luxembourg Income Study: Expanded metrics",
     "selection": [
         "Chile",
         "Brazil",
