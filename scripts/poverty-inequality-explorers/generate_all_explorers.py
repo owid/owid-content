9a5518921bf04a363a717dd731eba539ef7aa19f
Pablo Arriagada <paarriagadap@gmail.com>
2023-05-31 12:22:58 +0100

    remove PIP in title + remove keymetrics explorers

--- scripts/poverty-inequality-explorers/generate_all_explorers.py
+++ scripts/poverty-inequality-explorers/generate_all_explorers.py
@@ -6,7 +6,6 @@ from lis import (
     lis_expanded_poverty_explorer,
     lis_incomes_across_distribution_explorer,
     lis_inequality_explorer,
-    lis_keymetrics_explorer,
 )
 from multisource import (
     incomes_across_distribution_explorer_comparison,
@@ -21,8 +20,4 @@ from wbpip import (
     pip_poverty_explorer,
     pip_ppp_comparison_explorer,
 )
-from wid import (
-    wid_incomes_across_distribution_explorer,
-    wid_inequality_explorer,
-    wid_keymetrics_explorer,
-)
+from wid import wid_incomes_across_distribution_explorer, wid_inequality_explorer
