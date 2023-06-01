92dff02a0c8fad8f85ed206d0a9ed9fd6f5d8cf1
Pablo Arriagada <paarriagadap@gmail.com>
2023-06-01 17:55:05 +0100

    fix: Select default view for LIS poverty explorer

--- scripts/poverty-inequality-explorers/lis/lis_expanded_poverty_explorer.py
+++ scripts/poverty-inequality-explorers/lis/lis_expanded_poverty_explorer.py
@@ -1553,10 +1553,7 @@ df_graphers["mapTargetTime"] = df_graphers["mapTargetTime"].astype("Int64")
 # Select one default view
 df_graphers.loc[
     (df_graphers["Indicator Dropdown"] == "Share in poverty")
-    & (
-        df_graphers["Poverty line Dropdown"]
-        == "$2.15 per day: International Poverty Line"
-    )
+    & (df_graphers["Poverty line Dropdown"] == "$10 per day")
     & (df_graphers["Income measure Dropdown"] == "After tax vs. before tax")
     & (
         df_graphers["Adjust for household composition (equivalized income) Checkbox"]
