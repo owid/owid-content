13943581f3d660d9f06224198963525b020ab601
Pablo Arriagada <paarriagadap@gmail.com>
2023-09-22 15:45:56 +0100

    fix: line jumps

--- scripts/poverty-inequality-explorers/lis/lis_expanded_poverty_explorer.py
+++ scripts/poverty-inequality-explorers/lis/lis_expanded_poverty_explorer.py
@@ -91,7 +91,7 @@ sourceLink = "https://www.lisdatacenter.org/our-data/lis-database/"
 colorScaleNumericMinValue = 0
 tolerance = 5
 colorScaleEqualSizeBins = "true"
-new_line = "<br><br>"
+new_line = "\\n\\n"
 
 notes_title = "NOTES ON HOW WE PROCESSED THIS INDICATOR"
 
