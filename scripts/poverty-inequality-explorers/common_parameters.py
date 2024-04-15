b6c4f343bd3d247a44763e67c2723ee945c8460c
Pablo Arriagada <paarriagadap@gmail.com>
2024-04-15 12:20:05 -0400

    :sparkles: PIP update

--- scripts/poverty-inequality-explorers/common_parameters.py
+++ scripts/poverty-inequality-explorers/common_parameters.py
@@ -6,11 +6,11 @@ TOLERANCE = 5
 COLOR_SCALE_EQUAL_SIZEBINS = "true"
 NEW_LINE = "\\n\\n"
 Y_AXIS_MIN = 0
-MAP_TARGET_TIME = 2019
+MAP_TARGET_TIME = 0
 
 # WORLD BANK POVERTY AND INEQUALITY PLATFORM
-SOURCE_NAME_PIP = "World Bank Poverty and Inequality Platform (2023)"
-DATA_PUBLISHED_BY_PIP = "World Bank (2023). Poverty and Inequality Platform (version 20230919_2017 and 20230919_2011) [Data set]. World Bank Group. https://pip.worldbank.org/. Accessed January 17, 2024."
+SOURCE_NAME_PIP = "World Bank Poverty and Inequality Platform (2024)"
+DATA_PUBLISHED_BY_PIP = "World Bank (2024). Poverty and Inequality Platform (version 20240326_2017 and 20240326_2011) [Data set]. World Bank Group. https://pip.worldbank.org/. Accessed March 27, 2024."
 SOURCE_LINK_PIP = "https://pip.worldbank.org"
 CONSUMPTION_SPELLS_PIP = 7
 INCOME_SPELLS_PIP = 8
