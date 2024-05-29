5c90bbc41458967bcee159a631292159f60dcad6
Pablo Arriagada <paarriagadap@gmail.com>
2024-05-29 11:39:06 +0100

    :lipstick: distinguish sections

--- scripts/poverty-inequality-explorers/common_parameters.py
+++ scripts/poverty-inequality-explorers/common_parameters.py
@@ -1,6 +1,8 @@
 # NOTE: Don't forget to update the consumption and income spells for PIP
 
+####################################################################################################
 # COMMON PARAMETERS BETWEEN SOURCES
+####################################################################################################
 COLOR_SCALE_NUMERIC_MIN_VALUE = 0
 TOLERANCE = 5
 COLOR_SCALE_EQUAL_SIZEBINS = "true"
@@ -8,7 +10,9 @@ NEW_LINE = "\\n\\n"
 Y_AXIS_MIN = 0
 MAP_TARGET_TIME = 0
 
+####################################################################################################
 # WORLD BANK POVERTY AND INEQUALITY PLATFORM
+####################################################################################################
 SOURCE_NAME_PIP = "World Bank Poverty and Inequality Platform (2024)"
 DATA_PUBLISHED_BY_PIP = "World Bank (2024). Poverty and Inequality Platform (version 20240326_2017 and 20240326_2011) [Data set]. World Bank Group. https://pip.worldbank.org/. Accessed March 27, 2024."
 SOURCE_LINK_PIP = "https://pip.worldbank.org"
@@ -80,7 +84,9 @@ PROCESSING_DESCRIPTION_PIP_POVERTY = NEW_LINE.join(
 PPP_DESCRIPTION_PIP_2017 = "The data is measured in international-$ at 2017 prices – this adjusts for inflation and for differences in the cost of living between countries."
 PPP_DESCRIPTION_PIP_2011 = "The data is measured in international-$ at 2011 prices – this adjusts for inflation and for differences in the cost of living between countries."
 
+####################################################################################################
 # WORLD INEQUALITY DATABASE
+####################################################################################################
 SOURCE_NAME_WID = "World Inequality Database (WID.world) (2024)"
 DATA_PUBLISHED_BY_WID = "World Inequality Database (WID), https://wid.world"
 SOURCE_LINK_WID = "https://wid.world"
@@ -96,7 +102,9 @@ ADDITIONAL_DESCRIPTION_WID = NEW_LINE.join(
 )
 PPP_DESCRIPTION_WID = f"The data is measured in international-$ at {PPP_YEAR_WID} prices – this adjusts for inflation and for differences in the cost of living between countries."
 
+####################################################################################################
 # LUXEMBOURG INCOME STUDY
+####################################################################################################
 SOURCE_NAME_LIS = "Luxembourg Income Study (2023)"
 DATA_PUBLISHED_BY_LIS = "Luxembourg Income Study (LIS) Database, http://www.lisdatacenter.org (multiple countries; 1967-2021). Luxembourg, LIS."
 SOURCE_LINK_LIS = "https://www.lisdatacenter.org/our-data/lis-database/"
