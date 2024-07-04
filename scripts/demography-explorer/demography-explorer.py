d6a411357d1e7d5afa346db6d80fb055a53ecdb1
owidbot <tech@ourworldindata.org>
2024-07-04 11:05:51 +0000

    wip

--- scripts/demography-explorer/demography-explorer.py
+++ scripts/demography-explorer/demography-explorer.py
@@ -5,7 +5,8 @@ import textwrap
 import pandas as pd
 import re
 
-DATASET_PATH_PREFIX = "grapher/un/2024-07-11/un_wpp/"
+DATASET_PATH_PREFIX = "grapher/un/2024-07-11/un_wpp_full/"
+DATASET_PATH_PREFIX_ESTIMATES = "grapher/un/2024-07-11/un_wpp/"
 
 COLS_TO_DROP = []
 
