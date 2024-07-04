6bac29640eafc7da7ec30995bb090b3491962c73
owidbot <tech@ourworldindata.org>
2024-07-04 14:16:48 +0000

    wip

--- scripts/demography-explorer/demography-explorer.py
+++ scripts/demography-explorer/demography-explorer.py
@@ -110,7 +110,7 @@ for col in ["title", "subtitle"]:
         .apply(lambda x: x[0].upper() + x[1:] if len(x) else x)
         .apply(lambda x: re.sub(" {2,}", " ", x))
         # explicitly set empty strings to a single space, so we don't inherit it from ETL
-        .apply(lambda x: x or " ")
+        # .apply(lambda x: x or " ")
     )
 
 # %%
