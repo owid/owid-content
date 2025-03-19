85a1aceffc0c61b539573e27583ff0b3e31b3eca
Pablo Rosado <pabloarosado@gmail.com>
2025-03-19 08:52:08 +0100

    Use staging links to preview data

--- scripts/global-food-explorer/global-food-explorer.py
+++ scripts/global-food-explorer/global-food-explorer.py
@@ -26,6 +26,11 @@ outfile = "../../explorers/global-food.explorer.tsv"
 default_view = '`Food Dropdown` == "Maize (corn)" and `Metric Dropdown` == "Production" and `Per Capita Checkbox` == "false"'
 
 DATA_FILES_URL = f"https://catalog.ourworldindata.org/explorers/faostat/{VERSION}/food_explorer/"
+########################################################################################################################
+# TODO: Remove this temporary fix for local development.
+current_etl_branch = "data-agriculture-update"
+DATA_FILES_URL = f"http://staging-site-{current_etl_branch}:8881/explorers/faostat/{VERSION}/food_explorer/"
+########################################################################################################################
 
 # %%
 
